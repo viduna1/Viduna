8849777f895e6097e1de91939dec3ba56de051f1
Viduna Nethmira <81208043+viduna1@users.noreply.github.com>
2021-08-28 00:16:31 +0530

    Logo error fix

--- plugins/alogopack.js
+++ plugins/alogopack.js
@@ -34,13 +34,6 @@ else if (Config.WORKTYPE == 'public') {
     }));
     
 }
-const DRAGON= require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const axios = require('axios');
-const Config = require('../config');
-const need = "type some word after command\n🗡🗡"
-
 if (Config.WORKTYPE == 'private') {
 
     DRAGON.addCommand({ pattern: 'ahacking ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
