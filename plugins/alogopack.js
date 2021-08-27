5bbdde3b9cfcc440619037d47274152971e43e43
Viduna Nethmira <81208043+viduna1@users.noreply.github.com>
2021-08-27 23:24:38 +0530

    New logo

--- plugins/alogopack.js
+++ plugins/alogopack.js
@@ -34,3 +34,39 @@ else if (Config.WORKTYPE == 'public') {
     }));
     
 }
+/* # Exclusively from White-dragon🐲 
+*/
+
+const DRAGON= require('../events');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+const Config = require('../config');
+const need = "type some word after command\n🗡🗡"
+
+if (Config.WORKTYPE == 'private') {
+
+    DRAGON.addCommand({ pattern: 'ahacking ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/matrix?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    DRAGON.addCommand({ pattern: 'ahacking ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/matrix?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+    
+}
