175acb85a2319cd91d0f144b93c4b3bb61b2b86b
Viduna Nethmira <81208043+viduna1@users.noreply.github.com>
2021-08-28 00:48:06 +0530

    Five logos

--- plugins/alogopack.js
+++ plugins/alogopack.js
@@ -60,3 +60,107 @@ else if (Config.WORKTYPE == 'public') {
     }));
     
 }
+if (Config.WORKTYPE == 'private') {
+
+    DRAGON.addCommand({ pattern: 'aglow ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/breakwall?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    DRAGON.addCommand({ pattern: 'aglow ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/breakwall?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+    
+}
+if (Config.WORKTYPE == 'private') {
+
+    DRAGON.addCommand({ pattern: 'adropwater ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/dropwater?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    DRAGON.addCommand({ pattern: 'adropwater ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/dropwater?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+    
+}
+if (Config.WORKTYPE == 'private') {
+
+    DRAGON.addCommand({ pattern: 'awolf ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/wolflogo?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text1=White-Dragon&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    DRAGON.addCommand({ pattern: 'awolf ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/wolflogo?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text1=White-Dragon&text2=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+    
+}
+if (Config.WORKTYPE == 'private') {
+
+    DRAGON.addCommand({ pattern: 'aflower ?(.*)', fromMe: true,dontAddCommandList: true }, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/flowertext?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+}
+
+else if (Config.WORKTYPE == 'public') {
+
+    DRAGON.addCommand({ pattern: 'aflower ?(.*)', fromMe: false,dontAddCommandList: true}, (async (message, match) => {
+
+        if (match[1] === '') return await message.sendMessage(need);
+
+        var ttinullimage = await axios.get(`https://api.zeks.me/api/flowertext?apikey=tCDLKkjNNYrq1ATlMS0r0xa2taO&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
+
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: "White-dragon🐲" })
+
+    }));
+    
+}
