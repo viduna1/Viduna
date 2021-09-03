7add586cff8886ec69b8d67d705a13969c213ab9
Viduna Nethmira <81208043+viduna1@users.noreply.github.com>
2021-09-03 10:53:54 +0530

    Image update

--- whatsasena/Image.js
+++ whatsasena/Image.js
@@ -21,7 +21,7 @@ class Image extends Base {
         this.id = data.key.id === undefined ? undefined : data.key.id;
         this.jid = data.key.remoteJid;
         this.fromMe = data.key.fromMe;
-        this.caption = data.message.imageMessage.caption === null ? data.message.imageMessage.caption : '';
+        this.caption = data.message.imageMessage.caption === null ? data.message.imageMessage.caption : '*🇱🇰Created🐲By🐲White-Dragon🇱🇰*';
         this.url = data.message.imageMessage.url;
         this.timestamp = typeof(data.messageTimestamp) === 'object' ? data.messageTimestamp.low : data.messageTimestamp;
         this.mimetype = data.message.imageMessage.mimetype;
