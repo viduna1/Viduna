a62fc90e40acb2cfa154ffbbe23aa50bbaa65c48
Viduna Nethmira <81208043+viduna1@users.noreply.github.com>
2021-11-06 18:01:10 +0530

    Menu command change

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'dragon', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'alpha', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
